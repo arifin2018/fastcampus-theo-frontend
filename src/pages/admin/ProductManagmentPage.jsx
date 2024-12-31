7094f2c9a7c1b1453cfb97937164e4747a318c11
arifinlenna <arifin@lenna.ai>
2024-12-31 10:14:34 +0700

    update feature search

--- src/pages/admin/ProductManagmentPage.jsx
+++ src/pages/admin/ProductManagmentPage.jsx
@@ -4,7 +4,6 @@ import { IoAdd } from "react-icons/io5";
 import {
     Table,
     TableBody,
-    TableCaption,
     TableCell,
     TableFooter,
     TableHead,
@@ -14,26 +13,113 @@ import {
 import { useEffect, useState } from "react";
 import { axiosInstance } from "@/lib/axios";
 import { IoEllipsisHorizontal } from "react-icons/io5";
+import { useSearchParams } from "react-router";
+import {
+    Pagination,
+    PaginationContent,
+    PaginationItem,
+    PaginationLink,
+    PaginationNext,
+    PaginationPrevious,
+} from "@/components/ui/pagination"
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 
 
 const ProductManagementPage = () =>{
-    const [products,setProducts] = useState([])
+    const [products,setProducts] = useState({
+        data:[]
+    })
+    const [searchParams,setSearchParams] = useSearchParams()
+    const [page,setPage] = useState(1)
+    const [productSearch,setProductSearch] = useState('')
+    const [numberPagination,setNumberPagination] = useState([1])
 
+    function numberPages(number) {
+        setPage(number)
+    }
+
+    function handleSearchProduct() {
+        if (productSearch == "") {
+            searchParams.delete("name", productSearch)
+            setSearchParams(searchParams)
+        }else{
+            searchParams.set("name", productSearch)
+            setSearchParams(searchParams)
+        }
+        setPage(1)
+    }
+
+    function handleNextPage() {
+        setPage(Number(page)+1)
+    }
+
+    function handlePreviousPage() {
+        if (page > 1) {
+            searchParams.set("page", page-1)
+            setSearchParams(searchParams)
+            setPage(page-1)
+        }
+    }
+
+    function handleNumberPagination() {
+        const number = [];
+        let currentPage = page;
+        let totalPages = products.last;
+        const maxPagesToShow = 3; // Jumlah halaman yang akan ditampilkan di pagination
+        let startPage = Math.max(1, currentPage - Math.floor(maxPagesToShow / 2)); // Awal halaman
+
+        // Pastikan pagination tidak melebihi total halaman
+        if (startPage + maxPagesToShow - 1 > totalPages) {
+            startPage = Math.max(1, totalPages - maxPagesToShow + 1);
+        }
+
+        for (let i = startPage; i < startPage + maxPagesToShow && i <= totalPages; i++) {
+            number.push(i);
+        }
+
+        setNumberPagination(number)
+    }
 
     async function fetchProduct() {
-
+        let response
         try {
-            const response = await axiosInstance.get("/product")
+            response = await axiosInstance.get("/product",{
+                params:{
+                    _page:page,
+                    _per_page:2,
+                    name:productSearch
+                }
+            })
             setProducts(response.data)
-        } finally{
-            console.log("arifin");
+        }finally{
+            // console.info(products);
         }
     }
 
     useEffect(() => {
-        fetchProduct()
+        if (searchParams.get("page") == null) {
+            searchParams.set("page", 1)
+        }else{
+            searchParams.set("page", Number(searchParams.get("page")))
+            setPage(Number(searchParams.get("page")))
+        }
     },[])
+
+    useEffect(() => {
+        searchParams.set("page", page)
+        setSearchParams(searchParams)
+        fetchProduct()
+    },[page])
+
+    useEffect(() => {
+        fetchProduct()
+    },[searchParams])
+
+    useEffect(()=>{
+        handleNumberPagination()
+    },[products])
 
 
     return (
@@ -48,8 +134,16 @@ const ProductManagementPage = () =>{
                 </Button>}
             >
             <div>
-            <Table>
-                <TableCaption>A list of your recent invoices.</TableCaption>
+
+            <div className="mb-3">
+                <Label>SearchBar</Label>
+                <div className="flex">
+                    <Input className="w-1/4 h-8 mr-3" onChange={e => setProductSearch(e.target.value)} value={productSearch}></Input>
+                    <Button className="h-8" onClick={handleSearchProduct}>Search</Button>
+                </div>
+            </div>
+
+            <Table className="mb-3">
                 <TableHeader>
                     <TableRow>
                     <TableHead>Name</TableHead>
@@ -60,7 +154,7 @@ const ProductManagementPage = () =>{
                 </TableHeader>
                 <TableBody>
                     {
-                        products.map((product) => {
+                        products.data.map((product) => {
                             return (
                                 <TableRow key={product.id}>
                                     <TableCell>{product.name}</TableCell>
@@ -78,11 +172,42 @@ const ProductManagementPage = () =>{
                 </TableBody>
                 <TableFooter>
                     <TableRow>
-                    <TableCell colSpan={3}>Total</TableCell>
-                    <TableCell className="text-right">$2,500.00</TableCell>
+                    {/* <TableCell colSpan={3}>Total</TableCell>
+                    <TableCell className="text-right">$2,500.00</TableCell> */}
                     </TableRow>
                 </TableFooter>
             </Table>
+            <Pagination>
+                <PaginationContent>
+                    <PaginationItem>
+                        <Button disabled={page <= 1 }>
+                            <PaginationPrevious className="cursor-pointer" onClick={handlePreviousPage}/>
+                        </Button>
+                    </PaginationItem>
+                    <PaginationItem>
+                    </PaginationItem>
+                    <PaginationItem>
+                        {
+                            numberPagination.map((number) => (
+                                // href={`products?page=${number}`}
+                                <PaginationLink key={number} onClick={()=>{
+                                    numberPages(number)
+                                }} className={`cursor-pointer ${number == page ? 'bg-primary text-white hover:bg-gray-400' : ''}`}>
+                                    {number}
+                                </PaginationLink>
+                            ))
+                        }
+                    </PaginationItem>
+                    <PaginationItem>
+                    </PaginationItem>
+                    <PaginationItem>
+                        <Button disabled={page >= products.last}>
+                            <PaginationNext className="cursor-pointer" onClick={handleNextPage}/>
+                        </Button>
+                    </PaginationItem>
+                </PaginationContent>
+            </Pagination>
+
             </div>
             </AdminLayout>
         </>
