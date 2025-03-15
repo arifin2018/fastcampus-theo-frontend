86408db11475abfbb36f725bdcf573f36cff70ff
arifinlenna <arifin@lenna.ai>
2025-03-15 21:02:04 +0700

    update cart

--- src/components/pribadi/Cart.jsx
+++ src/components/pribadi/Cart.jsx
@@ -3,17 +3,17 @@ import { IoIosRemove } from "react-icons/io";
 import { IoIosAdd } from "react-icons/io";
 import { FcCheckmark } from "react-icons/fc";
 
-export default function Cart() {
+export default function Cart(props) {
     return (
-        <>
-            <div className="w-10/12 px-4">
-                <img src="https://placehold.co/600x400" alt="https://placehold.co/600x400" className="w-full" />
+        <div className="flex">
+            <div className="w-4/12 px-4">
+                <img src={props.imageUrl} alt={props.imageUrl} className="w-full" />
             </div>
 
             <div className="flex flex-col justify-center w-full">
                 <div className="flex justify-center flex-col">
-                    <p>600x400</p>
-                    <p className="font-bold">Rp. 200000</p>
+                    <p>{props.title}</p>
+                    <p className="font-bold">Rp. {props.price}</p>
                 </div>
                 <div className="flex justify-center gap-4">
                     <Button variants="ghost" size="icon">
@@ -36,6 +36,6 @@ export default function Cart() {
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
--- src/pages/carts.jsx
+++ src/pages/carts.jsx
@@ -1,16 +1,26 @@
 import { SigninPage } from "@/components/guards/SigninPage"
 import Cart from "@/components/pribadi/cart"
 import { Separator } from "@/components/ui/separator"
+import { useSelector } from "react-redux";
 
 export const Carts = () => {
+    const carts = useSelector(state => state.cart);
     return (
         <SigninPage>
             <main className="min-h-[78vh] max-w-screen-lg mx-auto w-full">
                 <h1 className="text-3xl font-bold flex justify-start">My Cart</h1>
                 <Separator/>
 
-                <div className="flex gap-4 mt-4">
-                    <Cart/>
+                <div className="flex flex-col gap-4 mt-4">
+                    {
+                        carts.data.map(
+                            (cart,index) => (
+                                <div className="" key={index}>
+                                    <Cart imageUrl={cart.product.ImageUrl} title={cart.product.name} price={cart.product.price}/>
+                                </div>
+                            )
+                        )
+                    }
                 </div>
             </main>
         </SigninPage>
